af3f29b9e9eda646a947418685b6205a22b3f04f
nakkajagadeesh12 <nakkajagadeesh1234@gmail.com>
2019-07-25 13:44:50 +0530

    1 file added

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from '../hero';
+
+export const HEROES: Hero[] = [
+  { id: 11, name: 'Jagadeesh' },
+  { id: 12, name: 'Bhaskar' },
+  { id: 13, name: 'Loknath' },
+  { id: 14, name: 'Valar' },
+  { id: 15, name: 'Veera' },
+  { id: 16, name: 'Rubin' },
+  { id: 17, name: 'Lakshmi' },
+  { id: 18, name: 'Sowjanya' },
+  { id: 19, name: 'Manish' },
+  { id: 20, name: 'Pavithra' }
+]
